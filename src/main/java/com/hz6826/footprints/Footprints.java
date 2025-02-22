f861c8a0e63f272c2964df6c1ab78e62cb7e27e2
Oak Platinum <oakplatinum@qq.com>
2025-02-22 18:06:20 +0800

    Initial Commit

---
+++ src/main/java/com/hz6826/footprints/Footprints.java
@@ -0,0 +1,16 @@
+package com.hz6826.footprints;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Footprints extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
